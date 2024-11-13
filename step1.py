42fc190366e6d8dce843bbfb8c0bf71aa22eea26
willy-10225 <swilly10225@gmail.com>
2024-11-13 16:38:56 +0800

    STEP1微調

--- step1.py
+++ step1.py
@@ -9,7 +9,10 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 orurl = "https://guide.michelin.com/"
-url = "https://guide.michelin.com/tw/zh_TW/restaurants/bib-gourmand"
+urllist = ['https://guide.michelin.com/tw/zh_TW/restaurants/3-stars-michelin',
+           "https://guide.michelin.com/tw/zh_TW/restaurants/2-stars-michelin",
+           "https://guide.michelin.com/tw/zh_TW/restaurants/1-star-michelin",
+           "https://guide.michelin.com/tw/zh_TW/restaurants/bib-gourmand"]
 # 设置 Chrome 驱动和无头模式
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -23,30 +26,12 @@ chrome_options.add_argument("--no-sandbox")  # 解决DevToolsActivePort文件不
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=chrome_options
 )
-
-# 打开网页
-driver.get(url)  # 替换为目标网址
-orlist = []
-# 等待特定元素加载完成
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (By.CSS_SELECTOR, "h1")
-        )  # 替换为实际的元素选择器
-    )
-    # 获取最终 HTML
-    final_html = driver.page_source
-    soup = BeautifulSoup(final_html, "html.parser")
-    a = soup.find("ul", class_="pagination")
-    b = [i.find("a").get("href") for i in a.find_all("li")][-2]
-    lastpage = int(b.split("/")[-1])
-    a = soup.find("div", class_="row restaurant__list-row js-restaurant__list_items")
-    orlist += [
-        orurl + i.find("a", class_="link").get("href")
-        for i in a.find_all("div", class_="col-md-6 col-lg-4 col-xl-3")
-    ]
-    for i in range(2, lastpage + 1):
-        driver.get(url + f"/page/{i}")
+for mainurl in urllist:
+    # 打开网页
+    driver.get(mainurl)  # 替换为目标网址
+    orlist = []
+    # 等待特定元素加载完成
+    try:
         element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located(
                 (By.CSS_SELECTOR, "h1")
@@ -55,22 +40,41 @@ try:
         # 获取最终 HTML
         final_html = driver.page_source
         soup = BeautifulSoup(final_html, "html.parser")
-        a = soup.find(
-            "div", class_="row restaurant__list-row js-restaurant__list_items"
-        )
+        a = soup.find("ul", class_="pagination")
+        b = [i.find("a").get("href") for i in a.find_all("li")][-2]
+        lastpage = int(b.split("/")[-1])
+        a = soup.find("div", class_="row restaurant__list-row js-restaurant__list_items")
         orlist += [
             orurl + i.find("a", class_="link").get("href")
             for i in a.find_all("div", class_="col-md-6 col-lg-4 col-xl-3")
         ]
-        print(len(orlist))
-except Exception as e:
-    print("出现错误:", e)
+        for i in range(2, lastpage + 1):
+            driver.get(mainurl + f"/page/{i}")
+            element = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.CSS_SELECTOR, "h1")
+                )  # 替换为实际的元素选择器
+            )
+            # 获取最终 HTML
+            final_html = driver.page_source
+            soup = BeautifulSoup(final_html, "html.parser")
+            a = soup.find(
+                "div", class_="row restaurant__list-row js-restaurant__list_items"
+            )
+            orlist += [
+                orurl + i.find("a", class_="link").get("href")
+                for i in a.find_all("div", class_="col-md-6 col-lg-4 col-xl-3")
+            ]
+            print(len(orlist))
+    except Exception as e:
+        print("出现错误:", e)
 
+    
+
+    # 读取 CSV 文件并将内容存储到 orlist 列表中，确保一行一条 URL
+    with open(rF".\step1\{mainurl.split('/')[-1]}.csv", mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        for url in orlist:
+            writer.writerow([url])  # 将每个 URL 作为独立的一行写入
 # 关闭浏览器
 driver.quit()
-
-# 读取 CSV 文件并将内容存储到 orlist 列表中，确保一行一条 URL
-with open(r".\url.csv", mode="w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    for url in orlist:
-        writer.writerow([url])  # 将每个 URL 作为独立的一行写入
